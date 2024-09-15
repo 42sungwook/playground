74bd19796e9b3f1effff56eefbe994bfa6b73da7
Sungwook Kim <silenc3@naver.com>
2024-09-15 14:37:09 +0900

    feat: add backend local docker

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 
 const app: Express = express()
-const port: number = 3000
+const port: number = parseInt(process.env.PORT || '4000', 10)
 
 app.use(cors())
 app.get('/api/hello', (req: Request, res: Response) => {
