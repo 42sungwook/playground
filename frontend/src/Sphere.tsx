41ec72ae57a7fcd21e84c5fad67a83a1174444ea
Sungwook Kim <silenc3@naver.com>
2024-09-20 16:25:32 +0900

    feat: add orbitControl

--- frontend/src/Sphere.tsx
+++ frontend/src/Sphere.tsx
@@ -1,19 +1,34 @@
-import { useFrame } from '@react-three/fiber'
+import { useFrame, useThree, extend, ReactThreeFiber } from '@react-three/fiber'
 import { useRef } from 'react'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+extend({ OrbitControls })
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    orbitControls: ReactThreeFiber.Object3DNode<
+      OrbitControls,
+      typeof OrbitControls
+    >
+  }
+}
 
 function Sphere() {
-  const cubeRef = useRef<THREE.Mesh>(null)
+  const sphereRef = useRef<THREE.Mesh>(null)
+  const { camera, gl } = useThree()
+  console.log(camera, gl)
 
   useFrame(() => {
-    if (cubeRef.current) {
-      cubeRef.current.rotation.y += 0.005
-      cubeRef.current.rotation.x += 0.005
+    if (sphereRef.current) {
+      sphereRef.current.rotation.y += 0.005
+      sphereRef.current.rotation.x += 0.005
     }
   })
   return (
     <>
-      <mesh ref={cubeRef}>
+      <orbitControls args={[camera, gl.domElement]} />
+      <mesh ref={sphereRef}>
         <sphereGeometry args={[1.5, 32, 32]} />
         <meshBasicMaterial
           color="mediumpurple"
